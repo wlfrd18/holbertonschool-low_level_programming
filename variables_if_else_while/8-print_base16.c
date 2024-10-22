3479a85bc3c2db39c18a4f100b4e6df39240c9da
wlfr18 <wilfriedpanol@gmail.com>
2024-10-22 14:16:23 +0000

    add anew file

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - entry poibt
+* Return: 0 if succes
+*/
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '16'; ++i)
+		if (i < 10)
+			putchar(i + '0');
+		else if
+			putchar(i - 10 + 'a');
+	putchar('\n')
+
+	return (0)
+}
+
