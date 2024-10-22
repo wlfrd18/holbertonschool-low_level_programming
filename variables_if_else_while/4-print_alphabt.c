1a49a76340653c5df0850425b993d116819653fe
wlfr18 <wilfriedpanol@gmail.com>
2024-10-22 13:19:37 +0000

    add anew file

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: 0 if succes
+*/
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		if ((a != 'e') && (a != 'q'));
+			{
+			putchar(c);
+			}
+	}
+	putchar(\n);
+	return (0)
+}
