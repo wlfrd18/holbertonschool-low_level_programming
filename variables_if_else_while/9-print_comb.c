bb625cf858b59857ac920bf86e7d53f652c8de4c
wlfr18 <wilfriedpanol@gmail.com>
2024-10-22 14:39:47 +0000

    add anew file

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: 0 if succes
+*/
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= 9; ++i)
+		putchar(i + '0');
+		if (i != '9')
+			putchar(',');
+			putchar(' ');
+	putchar('\n');
+
+	return (0);
+}
