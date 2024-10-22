91ca772cdb92ac6dda307b5671c234cf39c65de4
wlfr18 <wilfriedpanol@gmail.com>
2024-10-22 14:21:39 +0000

    add anew file

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -7,10 +7,10 @@ int main(void)
 {
 	int i;
 
-	for (i = '0'; i < '16'; ++i)
+	for (i = '0'; i < 16; ++i)
 		if (i < 10)
 			putchar(i + '0');
-		else if
+		else
 			putchar(i - 10 + 'a');
 	putchar('\n');
 
