f315fa60b90ce0d5d769846b61674b1ae4b69397
wlfr18 <wilfriedpanol@gmail.com>
2024-10-22 12:56:28 +0000

    add anew file

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: 0 if succes
+**/
+
+int main(void)
+{
+	char c;
+
+	c = 'a'
+	while (c <= 'z')
+	{
+		putchar(c);
+		++c;
+	}
+	c = 'A'
+	while (c <= 'Z')
+	{
+		putchar(c);
+		++i;
+	}
+	putchar('\n');
+	return (0);
+}
