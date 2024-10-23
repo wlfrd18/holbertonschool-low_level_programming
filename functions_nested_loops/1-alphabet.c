232ed1bb7e6c4983c8e565b575d7be724b6ea494
wlfrd18 <wilfriedpanol@gmail.com>
2024-10-23 12:01:57 +0000

    add a new file

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,12 +4,12 @@
 * a function that prints the alphabet, in lowercase, followed by a new line.
 * return: 0 if succes
 */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; ++letter)
-		_putchar('letter');
+		_putchar(letter);
 	_putchar('\n')
 
 	return (0)
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,13 @@
 #ifndef main_h
 #define main_h
 
+/*
+ * File: main.h
+ * Auth: Wilfried Panol Guele T
+ * Desc: Header file containing declarations for all functions
+ *       used in the 0x02-functions_nested_loops directory.
+ */
+
 int _putchar(char c);
 void print_alphabet(void);
 
