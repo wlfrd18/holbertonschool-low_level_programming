bafd208c068e6b35298f1dbfb35450a1a3040258
wlfrd18 <wilfriedpanol@gmail.com>
2024-10-23 13:40:48 +0000

    add a new file

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*
+*_isalpha - check if is a letter
+*
+* Return: 1 if succes, 0 if otherwise
+*/
+int _isalpha(int c)
+{
+	if (c >= 'a'; c <= 'z')
+		return (1);
+	if else (c >= 'A'; c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
