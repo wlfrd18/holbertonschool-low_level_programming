11a898014d61dc60ec92ed5288e2ffe0e1417da7
wlfrd18 <wilfriedpanol@gmail.com>
2024-10-23 14:04:10 +0000

    add a new file

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 * _islower - Checks if a character is lowercase.
 * @c: character to check.
 *
-* Return 1 if succes,  0 if iherwise.
+* Return: 1 if succes,  0 if iherwise.
 */
 int _islower(int c)
 {
