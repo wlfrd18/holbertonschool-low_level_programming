b0e4d79a0af8060542cbc0bc8d9671767df4604c
wlfrd18 <wilfriedpanol@gmail.com>
2024-10-23 13:16:28 +0000

    add a new file

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*
+* _islower - Entry point
+* function that checks for lowercase character
+* Return: 0 if succes
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
