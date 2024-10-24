8793738676b3fd2d4ab7aaa132ae77eb4192bf75
wlfrd18 <wilfriedpanol@gmail.com>
2024-10-24 12:02:28 +0000

    add anew file

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,14 +9,14 @@ void jack_bauer(void)
 {
 	int hour, minute, hour1, hour2, minute1, minute2;
 
-	for (hour = 0; hour <= 24; hour++)
+	for (hour = 0; hour < 24; hour++)
 	{
-	hour1 = hour / 10;
-	hour2 = hour % 10;
+		hour1 = hour / 10;
+		hour2 = hour % 10;
 		for (minute = 0; minute <= 59; minute++)
-		minute1 = minute / 10;
-		minute2 = minute % 10;
 		{
+			minute1 = minute / 10;
+			minute2 = minute % 10;
 			_putchar(hour1 + '0');
 			_putchar(hour2 + '0');
 			_putchar(':');
