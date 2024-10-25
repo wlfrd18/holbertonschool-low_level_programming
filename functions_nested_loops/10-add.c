1956c589416f5b7a37bc0f947fc7f34e1ac975b5
wlfrd18 <wilfriedpanol@gmail.com>
2024-10-25 11:03:16 +0000

    add a new file

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
 * add - adds two integers and returs the result
-*
+* @a: integer
+* @b: integer
 * Return: 0 if succees
 */
 int add(int a, int b)
 {
-	return(a +b);
+	return (a + b);
 
 }
