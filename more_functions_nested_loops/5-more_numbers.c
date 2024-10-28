771fdb42244054ee91c0fec29743716b6237da48
wlfrd18 <wilfriedpanol@gmail.com>
2024-10-28 13:17:38 +0000

    add a new file

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,13 @@
 
 void more_numbers(void)
 {
-	int i, j, k;
+	int i, j, k, l;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
-		k = j / 10
-		l = j % 10
+		k = j / 10;
+		l = j % 10;
 		{
 			if ( j >= 0 && j <= 9)
 				_putchar(j + '0');
