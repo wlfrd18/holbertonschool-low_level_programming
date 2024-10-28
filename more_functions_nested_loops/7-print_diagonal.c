2e1b3bcfb16ef526ab15396dc79298853694a24e
wlfrd18 <wilfriedpanol@gmail.com>
2024-10-28 16:21:13 +0000

    add a new file

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -5,16 +5,15 @@
 * Return: 0 always
 */
 
-/**
 void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n > 0)
+	if (n > '0')
 	{
-		for (i = 0; i < n; i++)
+		for (i = '0'; i < n; i++)
 		{
-			for (j = 0; j < i; j++)
+			for (j = '0'; j < i; j++)
 			{
 				_putchar(' ');
 			}
@@ -26,24 +25,4 @@ void print_diagonal(int n)
 	{
 	_putchar('\n');
 	}
-}
-*/
-void print_diagonal(int n)
-{
-	int i, j;
-
-if (n > 0)
-{
-	for (i = 0; i < n; i++)
-	{
-	for (j = 0; j < i; j++)
-	{
-		_putchar(' ');
-	}
-	_putchar('\\');
-	_putchar('\n');
-	}
-}
-else
-_putchar('\n');
 }
