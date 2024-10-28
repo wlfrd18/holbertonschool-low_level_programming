efbb1844ea17724cc5bea8c8f5889e311d753792
wlfrd18 <wilfriedpanol@gmail.com>
2024-10-28 15:54:35 +0000

    add a new file

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_diagonal - draws a diagonal line
+* @n: number of times of the character
+* Return: 0 always
+*/
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+		for (i = 0; i < n, i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+		_putchar('\');
+		_putchar('\n');
+		}
+	else
+	_putchar('\n');
+}
+
