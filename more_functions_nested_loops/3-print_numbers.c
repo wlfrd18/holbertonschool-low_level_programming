239b3c8c269ea013487799ec7c37f043babd487a
wlfrd18 <wilfriedpanol@gmail.com>
2024-10-28 12:03:07 +0000

    add a file

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_number - prints digit from 0 to 9
+*
+* Return: always 0
+*/
+
+void print_numbers(void)
+{
+	int i
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+}
