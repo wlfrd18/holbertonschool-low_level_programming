fb37d66e9a7fdf2fd86e8ddb74b7f16bad7f18a8
wlfrd18 <wilfriedpanol@gmail.com>
2024-10-31 11:48:20 +0000

    add anew file

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -32,4 +32,7 @@ void print_square(int size);
 void print_triangle(int size);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
 #endif
