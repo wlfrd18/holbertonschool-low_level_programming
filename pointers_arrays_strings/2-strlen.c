857efd92412ae0e9d8bbb84a818cc8dfe3c02593
wlfrd18 <wilfriedpanol@gmail.com>
2024-10-31 11:13:54 +0000

    add anew file

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string
+ * @s: char input
+ * Return: length of the input string
+ */
+int _strlen(char *s)
+{
+	int sum = 0;
+
+	for (i = '0'; i != '\0'; i++)
+	{
+		sum++
+	}
+	return (sum);
+}
