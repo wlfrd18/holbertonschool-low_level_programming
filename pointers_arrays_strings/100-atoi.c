cf5cf248290da92d030f6009e107469632f177f1
wlfrd18 <wilfriedpanol@gmail.com>
2024-11-03 13:59:44 +0000

    add new files

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: string to convert
+ * Return: the converted integer
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+	int started = 0;
+
+	while (*s)
+	{
+	if (*s == '-')
+		sign *= -1;
+	else if (*s == '+')
+		;
+	if (*s >= '0' && *s <= '9')
+	{
+		num = num * 10 + (*s - '0');
+		started = 1;
+	}
+	else if (started)
+		break;
+	s++;
+	}
+	return (sign * num);
+}
