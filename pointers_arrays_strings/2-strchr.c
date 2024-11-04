8eef55fb369fd7865ae08d9006ca5768d2be2ebd
wlfrd18 <wilfriedpanol@gmail.com>
2024-11-04 13:58:29 +0000

    add a new file

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* *_strchr - locates a charater in a string.
+* @*s: pointer to a string.
+* @c: character to check.
+* Return: a pointer to c if its found, and NUll otherwisse.
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+
+	return (NULL);
+}
