0b5d12181184523c70d08aea887df5aeff374701
wlfrd18 <wilfriedpanol@gmail.com>
2024-11-04 12:55:35 +0000

    add a new file

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* *_memset: fills memory with a constant byte.
+* @*s: pointer to the memory to fills
+* @b: constant character
+* @n: number of octet to fills
+* Return: pointer to the memory area s
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0, i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
