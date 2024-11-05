0c3ce8b4e3b2845ecec72124807335a79a19c157
wlfrd18 <wilfriedpanol@gmail.com>
2024-11-05 13:54:09 +0000

    add a new file

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <s:tdio.h>
 #include "main.h"
 
 /**
@@ -9,14 +9,14 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, total1 = 0, total2 = 0;
+	int i, total1 = 0, total2 = 0;
 
 
 	for (i = 0; i < size; i++)
 	{
-		total1 = total1 + a[i * size +i];
+		total1 = total1 + a[i * size + i];
 	}
-	for (j = 0; j < size; j++)
+	for (i = 0; i < size; i++)
 	{
 		total2 = total2 + a[i * size + (size - 1 - i)];
 	}
