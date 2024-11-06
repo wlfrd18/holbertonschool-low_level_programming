b45b6617bd19744a339b2dea962821ab996ae6eb
wlfrd18 <wilfriedpanol@gmail.com>
2024-11-06 14:20:52 +0000

    add a new file

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -8,13 +8,10 @@
 */
 void _puts_recursion(char *s)
 {
-	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
 	if (*s == '\0')
 		_putchar('\n');
+	else
+		_putchar(*s);
+		_puts_recursion(s + 1);
 }
