fceb3d253bdff9458bf9ce7f294b75363981e847
wlfrd18 <wilfriedpanol@gmail.com>
2024-11-06 15:17:55 +0000

    add a new file

--- recursion/main.h
+++ recursion/main.h
@@ -59,7 +59,7 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
-
+int _strlen_recursion(char *s);
 
 
 #endif
