61559be4ea6014a529b7b5fedf97ddbd5f506aca
wlfrd18 <wilfriedpanol@gmail.com>
2024-11-08 12:22:39 +0000

    add a new file

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - Entry point.
+* @argc: counter.
+* @argv: value.
+* Return: always 0.
+*/
+int main (int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+
+	return (0);
+}
+
