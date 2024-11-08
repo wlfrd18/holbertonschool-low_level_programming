361cbc9b9003f7f6f1a08e1311d9f61f2229b457
wlfrd18 <wilfriedpanol@gmail.com>
2024-11-08 14:01:57 +0000

    add a new file

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,14 +12,15 @@ int main(int argc, char *argv[])
 {
 	int a, b;
 
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
 	if (argc != 3)
 	{
-		printf("%s\n", "Error");
+		printf("Error\n");
 		return (1);
 	}
-	else
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	{
 		printf("%d\n", (a * b));
+	}
 	return (0);
 }
