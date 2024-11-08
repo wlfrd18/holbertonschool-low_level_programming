f8d40189a1ebef72d4ec666327d95f6bc5d9b743
wlfrd18 <wilfriedpanol@gmail.com>
2024-11-08 13:35:59 +0000

    add a new file

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -8,16 +8,16 @@
  * @argv: array pointing to arguments
  * Return: 0
  */
-int main(int argc, __attribute__((unused)) char *argv[])
+int main(int argc, char *argv[])
 {
 	int a, b;
+
 	a = atoi(argv[1]);
 	b = atoi(argv[2]);
-
 	if (argc != 3)
 	{
-                printf("%s\n", "Error");
-                return (1);
+		printf("%s\n", "Error");
+		return (1);
 	}
 	else
 		printf("%d\n", (a * b));
