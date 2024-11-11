7d73171988c3ab62ef0cd8fb45811e477b6d5e5e
wlfrd18 <wilfriedpanol@gmail.com>
2024-11-11 17:20:00 +0000

    add a new file

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - Concatenates two strings.
+ * @s1: The string to be concatenated upon.
+ * @s2: The string to be concatenated to s1.
+ * Return: NULL or a pointer.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *concat_str;
+	int index, concat_index = 0, len = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index]; index++)
+		len++;
+	for (index = 0; s1[index]; index++)
+		len++;
+
+	concat_str = malloc(sizeof(char) * (len + 1));
+
+	if (concat_str == NULL)
+		return (NULL);
+
+	for (index = 0; s1[index]; index++)
+		concat_str[concat_index++] = s1[index];
+
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s2[index];
+	concat_str[concat_index] = '\0';
+
+	return (concat_str);
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -59,5 +59,6 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
 
 #endif
