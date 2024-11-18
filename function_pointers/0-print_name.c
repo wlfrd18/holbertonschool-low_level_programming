20989c8d672b655b3844020136eb86687cea8421
wlfrd18 <wilfriedpanol@gmail.com>
2024-11-18 11:28:16 +0000

    add a new file

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints the name
+ * @name: name
+ * @f: function
+ */
+
+ void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
