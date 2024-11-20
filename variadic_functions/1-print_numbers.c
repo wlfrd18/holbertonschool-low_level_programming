00e33e71cb6d08fbc5776deafd409d14d7fa1c99
wlfrd18 <wilfriedpanol@gmail.com>
2024-11-20 21:12:53 +0000

    modify the incrementation style of  the task 1 of the C-Variadic functions

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(nums, n);
 
-	for (index = 0; index < n; index++)
+	for (index = 0; index < n; ++index)
 	{
 		printf("%d", va_arg(nums, int));
 
