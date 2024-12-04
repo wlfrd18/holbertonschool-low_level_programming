cd24cf2565d74004d1ab825c38e602435fa7e498
wlfrd18 <wilfriedpanol@gmail.com>
2024-12-04 17:09:40 +0000

    fix my function of thr task 0

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -14,11 +14,11 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (h != NULL)
 	{
-		printf("%d"\n, h->n);
+		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
-	return (count)
+	return (count);
 }
 
 
